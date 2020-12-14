1cfa289ba19cc11fc30f43a49c4a4fa782c74139
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2020-12-14 13:09:50 +0100

    Add files via upload

--- adventure.js
+++ adventure.js
@@ -1,240 +1,240 @@
-const description = document.getElementById('description')
-const buttons = document.getElementById('game-buttons')
-var image = document.getElementById("inventoryItem");
+//Variabelen
+var gamecontainer = document.getElementById("game-container");
+var title = document.getElementById("title");
+var description = document.getElementById("description");
+var gamebuttons = document.getElementById("game-buttons");
+var button1 = document.getElementById("button1");
+var button2 = document.getElementById("button2");
+var button3 = document.getElementById("button3");
+var inventoryitem = document.getElementById("inventoryItem");
 
-let state = {}
+//Laat de code starten
+Start();
 
-function startGame() {
-  state = {}
-  showtekst(1)
+//Begin scherm
+function Start(){
+  gamecontainer.className = "Start Game";
+  title.innerHTML = "Escape the forest.";
+  description.innerHTML = "In deze game kom je verder door de juiste keuzes te maken.";
+
+  button1.innerHTML = "Start Game!";
+  button1.style.display = "inline";
+  button1.onclick = begin;
+
+  button2.style.display = "none";
+
+  button3.style.display = "none";
+
+  inventoryitem.style.display = "none";
 }
 
-function showtekst(tekstIndex) {
-  const tekst = teksten.find(tekst => tekst.id === tekstIndex)
-  description.innerText = tekst.text
-  image.src="Images/bos.jpg"
-  while (buttons.firstChild) {
-    buttons.removeChild(buttons.firstChild)
-  }
+//Scherm begin
+function begin(){
+  gamecontainer.className = "begin";
+  title.innerHTML = "Escape the forest.";
+  description.innerHTML = "Het is nacht en je bent verdwaald in een bos en staat op een splitsing welke kant kies je?";
 
-  tekst.options.forEach(option => {
-    if (showOption(option)) {
-      const button = document.createElement('button')
-      button.innerText = option.text
-      button.classList.add('btn')
-      button.addEventListener('click', () => selectOption(option))
-      buttons.appendChild(button)
-    }
-  })
+  button1.innerHTML = "Ga naar links.";
+  button1.style.display = "inline";
+  button1.onclick = huis;
+
+  button2.innerHTML = "Ga naar rechts.";
+  button2.style.display = "inline";
+  button2.onclick = voorbijHuis;
+
+  button3.style.display = "none";
+
+  inventoryitem.style.display = "none";
 }
 
-function showOption(option) {
-  return option.requiredState == null || option.requiredState(state)
+//Scherm huis
+function huis(){
+  gamecontainer.className = "huis";
+  title.innerHTML = "Escape the forest.";
+  description.innerHTML = "Je komt aan bij een huis je ziet licht branden in het huis wat doe je?";
+
+  button1.innerHTML = "Ga naar binnen.";
+  button1.style.display = "inline";
+  button1.onclick = huis2;
+
+  button2.innerHTML = "Loop voorbij het huis.";
+  button2.style.display = "inline";
+  button2.onclick = voorbijHuis;
+
+  button3.style.display = "none";
+
+  inventoryitem.style.display = "none";
 }
 
-function selectOption(option) {
-  const nexttekstId = option.nextText
-  if (nexttekstId <= 0) {
-    return startGame()
-  }
-  state = Object.assign(state, option.setState)
-  showtekst(nexttekstId)
+//Scherm huis2
+function huis2(){
+  gamecontainer.className = "huis2";
+  title.innerHTML = "Escape the forest.";
+  description.innerHTML = "Je hebt geen sleutel dus je kunt het huis niet betreden.";
+
+  button1.innerHTML = "Ga terug.";
+  button1.style.display = "inline";
+  button1.onclick = voorbijHuis;
+
+  button2.style.display = "none";
+
+  button3.style.display = "none";
+
+  inventoryitem.style.display = "none";
 }
 
-const teksten = [
-  {
-      id: 1,
-      text: 'In deze game kom je verder door de juiste keuze te maken.',
-      options: [
-        {
-          text: 'Start',
-          nextText: 14
-        }
-      ]
-    },
-    {
-    id: 14,
-    text: 'Het is nacht en je bent verdwaald in een bos en staat op een splitsing welke kant kies je?',
-    options: [
-      {
-        text: 'Ga naar links',
-        setState: { sleutel: false },
-        nextText: 2
-      },
-      {
-        text: 'Ga naar rechts',
-        nextText: 3
-      }
-    ]
-  },
-  {
-    id: 2,
-    text: 'Je komt aan bij een huis je ziet licht branden in het huis wat doe je?',
-    options: [
-      {
-        text: 'Ga naar binnen',
-        requiredState: (currentState) => currentState.sleutel,
-        setState: { sleutel: true },
-        nextText: 5
-      },
-      {
-        text: 'Loop voorbij het huis',
-        nextText: 10
-      },
-     
-    ]
-  },
-  {
-    id: 3,
-    text: 'Je loopt richting een grot vlak naast de grot zie je een zwaard wat doe je?',
-    options: [
-      {
-        text: 'Pak het zwaard',
-        setState: { zwaard: true },
-        nextText: 4
-      },
-      {
-        text: 'Laat het zwaard liggen',
-        nextText: 7
-      },
-     
-    ]
-  },
-  {
-    id: 4,
-    text: 'In de grot kom je een beer tegen wat doe je?',
-    options: [
-      {
-        text: 'Vermoord de beer',
-        requiredState: (currentState) => currentState.zwaard,
-        setState: { zwaard: true },
-        nextText: 6
-        },
-        {
-        text: 'Probeer langs de beer te sluipen',
-        nextText: 6
-      },
-      {
-        text: 'Loop terug de grot uit.',
-        nextText: 15
-        },
-    ]
-  },
-    {
-  id: 7,
-  text: 'In de grot kom je oog in oog te staan met een beer dit overleef je niet.',
-  options: [
-    {
-      requiredState: (currentState) => currentState.zwaard,
-      setState: { zwaard: false },
-      nextText: 5
-      },
-      {
-      text: 'Restart',
-      nextText: -1
-    }
-  ]
-},
-{
-  id: 15,
-  text: 'Terwijl je naar de uitgang loopt struikel je over een steentje vervolgens wordt je vermoord door de beer.',
-  options: [
-    {
-      text: 'Game Over!',
-      nextText: -1
-    }
-  ]
-},
-  {
-    id: 5,
-    text: 'Je bent vermoord door de beer.',
-    options: [
-      {
-        text: 'Game Over!',
-        nextText: -1
-      }
-    ]
-  },
-  {
-    id: 6,
-    text: 'Het is je gelukt om door de grot heen te komen! Je ziet een kampvuur wat doe je?',
-    options: [
-      {
-        text: 'Rust  uit bij het kampvuur',
-        nextText: 8
-      },
-    {
-        text: 'Ga verder.',
-        nextText: 9
-    }
-    ]
-  },
-  { id: 9,
-    text: 'Je bent oververmoeid geraakt.',
-    options: [
-      {
-        text: 'Game Over!',
-        nextText: -1
-      }
-    ]
-  },
-  { id: 8,
-    text: 'Na het uitrusten kom je aan bij een dorpje. Het is je gelukt om uit het bos te komen!',
-    options: [
-      {
-        text: 'You Win!',
-        nextText: -1
-      }
-    ]
-  },
-  {
-    id: 10,
-    text: 'Na een paar minuten wandelen zie je een sleutel op de grond liggen.',
-    options: [
-      {
-        text: 'Pak de sleutel en ga terug naar het huisje.',
-        setState: { sleutel: true },
-        nextText: 11
-      },
-    {
-        text: 'Ga verder.',
-        nextText: 13
-    }
-    ]
-  },
-  {
-    id: 11,
-    text: 'Na een paar minuten wandelen ben je weer bij het huisje aangekomen.',
-    options: [
-      {
-        text: 'Ga naar binnen.',
-        equiredState: (currentState) => currentState.sleutel,
-        setState: { sleutel: true },
-        nextText: 12
-    }
-    ]
-  },
-  { id: 12,
-    text: 'In het huisje staat wat te eten en te drinken klaar, Je hebt de nacht overleefd!',
-    options: [
-      {
-        text: 'You Win!',
-        nextText: -1
-      }
-    ]
-  },
-  { id: 13,
-    text: 'Je bent uitgehongerd geraakt.',
-    options: [
-      {
-        text: 'Game Over!',
-        nextText: -1
-      }
-    ]
-  },
-]
+//Scherm voorbij huis
+function voorbijHuis(){
+  gamecontainer.className = "voorbijHuis";
+  title.innerHTML = "Escape the forest.";
+  description.innerHTML = "Je loopt richting een grot vlak naast de grot zie je een zwaard wat doe je?";
 
-startGame()
+  button1.innerHTML = "Pak het zwaard.";
+  button1.style.display = "inline";
+  button1.onclick = grot2;
+
+  button2.innerHTML = "Laat het zwaard liggen.";
+  button2.style.display = "inline";
+  button2.onclick = grot;
+
+  button3.style.display = "none";
+
+  inventoryitem.style.display = "none";
+}
+
+//Scherm grot
+function grot(){
+  gamecontainer.className = "grot";
+  title.innerHTML = "Escape the forest.";
+  description.innerHTML = "In de grot kom je een beer tegen wat doe je?";
+
+  button1.innerHTML = "Probeer langs de beer te sluipen.";
+  button1.style.display = "inline";
+  button1.onclick = vermoord2;
+
+  button2.innerHTML = "Vermoord de beer.";
+  button2.style.display = "inline";
+  button2.onclick = vermoord;
+
+  button3.innerHTML = "Loop terug de grot uit.";
+  button3.style.display = "inline";
+  button3.onclick = struikel;
+
+  inventoryitem.style.display = "none";
+}
+
+//Scherm grot2
+function grot2(){
+  gamecontainer.className = "grot2";
+  title.innerHTML = "Escape the forest.";
+  description.innerHTML = "In de grot kom je een beer tegen wat doe je?";
+
+  button1.innerHTML = "Probeer langs de beer te sluipen.";
+  button1.style.display = "inline";
+  button1.onclick = vermoord2;
+
+  button2.innerHTML = "Vermoord de beer.";
+  button2.style.display = "inline";
+  button2.onclick = succeed;
+
+  button3.innerHTML = "Loop terug de grot uit.";
+  button3.style.display = "inline";
+  button3.onclick = struikel;
+
+  inventoryitem.style.display = "none";
+}
+
+//Schrem struikel
+function struikel(){
+  gamecontainer.className = "struikel";
+  title.innerHTML = "Escape the forest.";
+  description.innerHTML = "Terwijl je naar de uitgang loopt struikel je over een steentje vervolgens wordt je vermoord door de beer!";
+
+  button1.innerHTML = "Play again!";
+  button1.style.display = "inline";
+  button1.onclick = Start;
+
+  button2.style.display = "none";
+
+  button3.style.display = "none";
+}
+
+//Scherm vermoord
+function vermoord(){
+  gamecontainer.className = "vermoord";
+  title.innerHTML = "Escape the forest.";
+  description.innerHTML = "Je bent vermoord door de beer!";
+    
+  button1.innerHTML = "Play again!";
+  button1.style.display = "inline";
+  button1.onclick = Start;
+
+  button2.style.display = "none";
+
+  button3.style.display = "none";
+}
+
+//Scherm vermoord2
+function vermoord2(){
+  gamecontainer.className = "vermoord2";
+  title.innerHTML = "Escape the forest.";
+  description.innerHTML = "In de grot kom je oog in oog te staan met een beer dit overleef je niet!";
+
+  button1.innerHTML = "Play again!";
+  button1.style.display = "inline";
+  button1.onclick = Start;
+
+  button2.style.display = "none";
+
+  button3.style.display = "none";
+}
+
+//Scherm succeed
+function succeed(){
+  gamecontainer.className = "succeed";
+  title.innerHTML = "Escape the forest.";
+  description.innerHTML = "Het is je gelukt om door de grot heen te komen! Je ziet een kampvuur wat doe je?";
+
+  button1.innerHTML = "Rust uit bij het kampvuur";
+  button1.style.display = "inline";
+  button1.onclick = succeed2;
+
+  button2.innerHTML = "Ga verder.";
+  button2.style.display = "inline";
+  button2.onclick = oververmoeid;
+
+  button3.style.display = "none";
+
+  inventoryitem.style.display = "none";
+}
+
+//Scherm oververmoeid
+function oververmoeid(){
+  gamecontainer.className = "oververmoeid";
+  title.innerHTML = "Escape the forest.";
+  description.innerHTML = "Je bent oververmoeid geraakt!";
+
+  button1.innerHTML = "Play again!";
+  button1.style.display = "inline";
+  button1.onclick = Start;
+
+  button2.style.display = "none";
+
+  button3.style.display = "none";
+}
 
 
+//Scherm succeed2
+function succeed2(){
+  gamecontainer.className = "succeed2";
+  title.innerHTML = "Escape the forest.";
+  description.innerHTML = "Na het uitrusten kom je aan bij een dorpje. Het is je gelukt om uit het bos te komen!";
 
+  button1.innerHTML = "Play again!";
+  button1.style.display = "inline";
+  button1.onclick = Start;
+
+  button2.style.display = "none";
+
+  button3.style.display = "none";
+}
