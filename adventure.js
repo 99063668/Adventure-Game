bd6acebfbe14f2f930ca60591eedcfa8d6cd0113
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2020-12-17 09:45:48 +0100

    Add files via upload

--- adventure.js
+++ adventure.js
@@ -7,13 +7,12 @@ var button1 = document.getElementById("button1");
 var button2 = document.getElementById("button2");
 var button3 = document.getElementById("button3");
 var inventoryitem = document.getElementById("inventoryItem");
-var oppakken = {Sleutel:false, Zwaard:false, Shotgun:false, Slenderman:false, Sirenhead:false};
+var oppakken = {Sleutel:false, Zwaard:false, Shotgun:false, Slenderman:false};
 
-var a = oppakken["Sleutel"];
 //Geluid
-/*var myMusic = new Audio()
+var myMusic = new Audio()
 myMusic.src = "sound.mp3"
-myMusic.play();*/
+myMusic.play();
 
 //Laat de code starten
 Start();
@@ -50,11 +49,7 @@ function Start(){
   title.innerHTML = "Escape the forest.";
   description.innerHTML = "In deze game kom je verder door de juiste keuzes te maken.";
 
-  setButtons("Start game", begin, null);
-
-  button2.style.display = "none";
-
-  button3.style.display = "none";
+  setButtons("Start game", begin, null, null);
 
   inventoryitem.style.display = "none";
 }
@@ -65,10 +60,7 @@ function begin(){
   title.innerHTML = "De splitsing.";
   description.innerHTML = "Het is nacht en je bent verdwaald in een bos je staat op een splitsing je kunt 2 kanten op, welke kant kies je?";
 
-  setButtons("Ga naar links", huis, "Ga naar rechts", voorbijHuis);
-
-
-  button3.style.display = "none";
+  setButtons("Ga naar links", huis, "Ga naar rechts", voorbijHuis, null);
 
   inventoryitem.style.display = "none";
 }
@@ -79,10 +71,7 @@ function huis(){
   title.innerHTML = "Het huis.";
   description.innerHTML = "Na een paar minuten lopen kom je aan bij een huis. In het huis zie je licht branden het ziet ernaar uit dat er niemand in het huis is wat doe je?";
 
-  setButtons("Ga naar binnen", huis2, "Loop voorbij het huis", voorbijHuis);
-
-
-  button3.style.display = "none";
+  setButtons("Ga naar binnen", huis2, "Loop voorbij het huis", voorbijHuis, null);
 
   inventoryitem.style.display = "none";
 }
@@ -93,9 +82,7 @@ function huis2(){
   title.innerHTML = "De sleutel.";
   description.innerHTML = "De deur zit opslot en je hebt geen sleutel dus je kunt het huis niet betreden.";
 
-  setButtons("Ga terug", terug, null);
-
-  button3.style.display = "none";
+  setButtons("Ga terug", terug, null, null);
 
   inventoryitem.style.display = "none";
 }
@@ -106,12 +93,11 @@ function terug(){
   title.innerHTML = "De sleutel.";
   description.innerHTML = "Na een paar minuten wandelen zie je een sleutel op de grond liggen.";
 
-  setButtons("Pak de sleutel en ga terug naar het huisje", huisje, "Ga verder zonder sleutel", uitgehongerd);
-
-  button3.style.display = "none";
+  setButtons("Pak de sleutel en ga terug naar het huisje", huisje, "Ga verder zonder sleutel", uitgehongerd, null);
 
   inventoryitem.style.display = "inline";
   inventoryitem.src= "Images/key.png";
+
 
   inventoryitem.onclick = function () {
     oppakken["Sleutel"] = true;
@@ -137,9 +123,7 @@ function huisje(){
   title.innerHTML = "Het huis.";
   description.innerHTML = "Na een paar minuten wandelen ben je weer bij het huisje aangekomen. Het lijkt als of er iemand aanwezig is rondom het huis.";
 
-  setButtons("Ga naar binnen", succeed3, "Probeer Sirenhead te vermoorden", sirenhead);
-
-  button3.style.display = "none";
+  setButtons("Ga naar binnen", succeed3, "Probeer Sirenhead te vermoorden", sirenhead, null);
 
   inventoryitem.style.display = "none";
 }
@@ -150,9 +134,7 @@ function voorbijHuis(){
   title.innerHTML = "De grot.";
   description.innerHTML = "Je loopt richting een grot vlak naast de grot zie je een zwaard liggen wat doe je?";
 
-  setButtons("Ga de grot in", grot2, null);
- 
-  button3.style.display = "none";
+  setButtons("Ga de grot in", grot2, null), null;
 
   inventoryitem.style.display = "inline";
   inventoryitem.src= "Images/zwaard.png";
@@ -199,11 +181,7 @@ function struikel(){
   title.innerHTML = "You Lose!";
   description.innerHTML = "Terwijl je naar de uitgang loopt struikel je over een steentje vervolgens wordt je vermoord door de beer!";
 
-  setButtons("Play again!", Start, null);
-
-  button2.style.display = "none";
-
-  button3.style.display = "none";
+  setButtons("Play again!", Start, null, null);
 }
 
 //Scherm vermoord
@@ -212,11 +190,7 @@ function vermoord(){
   title.innerHTML = "You Lose!";
   description.innerHTML = "Je bent vermoord door de beer!";
     
-  setButtons("Play again!", Start, null);
-
-  button2.style.display = "none";
-
-  button3.style.display = "none";
+  setButtons("Play again!", Start, null, null);
 }
 
 //Scherm succeed
@@ -225,11 +199,7 @@ function succeed(){
   title.innerHTML = "You Win!";
   description.innerHTML = "Het is je gelukt om door de grot heen te komen! Je ziet een kampvuur wat doe je?";
 
-  setButtons("Rust uit bij het kampvuur", succeed2, "Ga verder", oververmoeid);
- 
-  button3.style.display = "none";
-
-  inventoryitem.style.display = "none";
+  setButtons("Rust uit bij het kampvuur", succeed2, "Ga verder", oververmoeid, null);
 }
 
 //Scherm oververmoeid
@@ -238,11 +208,7 @@ function oververmoeid(){
   title.innerHTML = "You Lose!";
   description.innerHTML = "Je bent oververmoeid geraakt!";
 
-  setButtons("Play again!", Start, null);
-
-  button3.style.display = "none";
-
-  inventoryitem.style.display= "none";
+  setButtons("Play again!", Start, null, null);
 }
 
 //Scherm succeed2
@@ -251,9 +217,7 @@ function succeed2(){
   title.innerHTML = "De vreemde man";
   description.innerHTML = "Na het uitrusten loop je verder door het bos je ziet een vreemde man wat doe je?.";
 
-  setButtons("Ren weg", oververmoeid, "Verdedig jezelf", slenderman);
-
-  button3.style.display = "none";
+  setButtons("Ren weg", oververmoeid, "Verdedig jezelf", slenderman, null);
 
   inventoryitem.style.display = "inline";
   inventoryitem.src= "Images/shotgun.png";
@@ -282,9 +246,7 @@ function slenderman(){
   title.innerHTML = "Slenderman.";
   description.innerHTML = "Met de shotgun die je net hebt opgepakt kun je Slenderman vermoorden.";
 
-  setButtons("Ga verder", slenderman2, null);
-
-  button3.style.display = "none";
+  setButtons("Ga verder", slenderman2, null, null);
 
   inventoryitem.style.display = "none";
   inventoryitem.src= "Images/slender.png";
@@ -313,11 +275,7 @@ function slenderman2(){
   title.innerHTML = "You win!.";
   description.innerHTML = "Je hebt Slenderman vermoord en hebt veilig het eind van het bos bereikt.";
 
-  setButtons("Play again!", Start, null);
-
-  button3.style.display = "none";
-
-  inventoryitem.style.display = "none";
+  setButtons("Play again!", Start, null, null);
 }
 
   //Scherm uitgehongerd
@@ -326,13 +284,7 @@ function uitgehongerd(){
   title.innerHTML = "You Lose!";
   description.innerHTML = "Je bent uitgehongerd geraakt!";
 
-  setButtons("Play again!", Start, null);
-
-  button2.style.display = "none";
-
-  button3.style.display = "none";
-
-  inventoryitem.style.display= "none";
+  setButtons("Play again!", Start, null), null;
 }
 
 //Scherm succed3
@@ -341,13 +293,8 @@ function succeed3(){
   title.innerHTML = "You Win!";
   description.innerHTML = "In het huisje staat wat te eten en te drinken klaar, Je hebt de nacht overleefd!";
 
-  setButtons("Play again!", Start, null);
-
-  button2.style.display = "none";
-
-  button3.style.display = "none";
-
-  inventoryitem.style.display = "none";}
+  setButtons("Play again!", Start, null, null);
+}
 
 //Sirenhead
 function sirenhead(){
@@ -355,9 +302,5 @@ function sirenhead(){
   title.innerHTML = "You lose!";
   description.innerHTML = "Je probeerde Sirenhead aantevallen, dit heb je niet overleefd!";
 
-  setButtons("Play again!", Start, null);
-
-  button2.style.display = "none";
-
-  button3.style.display = "none";
+  setButtons("Play again!", Start, null, null);
 }
